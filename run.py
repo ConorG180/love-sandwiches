8e288c9ab47d3f0221b4a7a60fc1ce65c3d8a190
Conor <conor.ganly@yahoo.com>
2022-05-14 15:39:31 +0000

    Create function to get sales data from spreadsheet

--- run.py
+++ run.py
@@ -12,11 +12,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet("sales")
 
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    Get sales data from the user
+    """
+    print("Please enter sales data from the previous market.")
+    print("Data should be 6 numbers separated by commas")
+    print("For example, 24,17,31,22,27,19/n")
+    data_str = input("Enter data here")
+    print(f"the data you have entered is {data_str}")
 
-print(data)
+
+get_sales_data()
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
