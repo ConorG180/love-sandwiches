4295ea5e77573c7354bcb923dab355d631dfafaf
Conor <conor.ganly@yahoo.com>
2022-05-14 17:12:05 +0000

    Add function to validate data

--- run.py
+++ run.py
@@ -22,6 +22,22 @@ def get_sales_data():
     print("For example, 24,17,31,22,27,19/n")
     data_str = input("Enter data here")
     print(f"the data you have entered is {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(data):
+    """
+    Converts user input to int data type and also validates
+    the data - checks if user input is of exactly 6 values
+    """
+    try:
+        if len(data) != 6:
+            raise ValueError(
+                f"6 values expected but {len(data)} were given"
+            )
+    except ValueError as e:
+        print(f"invalid data Hombre! - {e}, give it another go please.")
 
 
 get_sales_data()
