222c73b3c3317e44c99b70910f21e8b4d543093a
Conor <conor.ganly@yahoo.com>
2022-05-15 14:30:34 +0000

    Add function to update sales worksheet

--- run.py
+++ run.py
@@ -17,13 +17,17 @@ def get_sales_data():
     """
     Get sales data from the user
     """
-    print("Please enter sales data from the previous market.")
-    print("Data should be 6 numbers separated by commas")
-    print("For example, 24,17,31,22,27,19/n")
-    data_str = input("Enter data here")
-    print(f"the data you have entered is {data_str}")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    while True:
+        print("Please enter sales data from the previous market.")
+        print("Data should be 6 numbers separated by commas")
+        print("For example, 24,17,31,22,27,19\n")
+        data_str = input("Enter data here")
+        print(f"the data you have entered is {data_str}")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            break
+
+    return sales_data
 
 
 def validate_data(data):
@@ -32,15 +36,31 @@ def validate_data(data):
     the data - checks if user input is of exactly 6 values
     """
     try:
+        data = [int(i) for i in data]
         if len(data) != 6:
             raise ValueError(
                 f"6 values expected but {len(data)} were given"
             )
+        print("data valid Hermano!")
+        return True
     except ValueError as e:
         print(f"invalid data Hombre! - {e}, give it another go please.")
+        return False
 
 
-get_sales_data()
+def update_sales_worksheet(user_inputs):
+    """
+    Function for updating the sales spreadsheet
+    """
+    print("Updating sales worksheet")
+    sales_sheet = SHEET.worksheet("sales")
+    sales_sheet.append_row(user_inputs)
+    print("Sales worksheet Updated!")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(data)
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
